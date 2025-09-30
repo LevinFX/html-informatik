005d0f51528a682d07f80092e60656202d4bef5a
LevinFX <hp.hogwarts.fan@web.de>
2025-09-30 17:40:42 +0200

    removed story tracking functionality and cleaned up a bit after minimizing scope of project

--- abenteuer/js/debugger.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Trace:', GameState.getTraceLevel());
-  console.log('Tools:', GameState.getList('toolset'));
-  console.log('Files:', GameState.getList('files'));
-  console.log('DB?', GameState.get('dbAccess'));
-});
